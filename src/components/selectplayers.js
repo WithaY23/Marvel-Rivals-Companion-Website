95243bb7fdffc9b460be0846f2fe4503d04c7d55
Ayden Budhoo <abudhoo@cs.stonybrook.edu>
2026-01-07 12:08:14 -0500

    Basic implementations of selectroles NOT COMPLETED

--- src/components/selectplayers.js
+++ src/components/selectplayers.js
@@ -7,7 +7,7 @@ export default function SelectPlayers(props)
             <h1>How Many Players Are In Your Lobby?</h1>
 
             <div className="select-players-buttons-container">
-                <button className="select-players-button">1</button>
+                <button className="select-players-button" onClick={() => {}}>1</button>
                 <button className="select-players-button">2</button>
                 <button className="select-players-button">3</button>
                 <button className="select-players-button">4</button>
